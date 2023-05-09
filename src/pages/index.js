ba829c9e1a5a02fb27c2fa35fbedc6f4429d58ad
Meet1501 <meet.infynno@gmail.com>
2023-05-09 16:32:30 +0530

    fix : next seo image url

--- src/pages/index.js
+++ src/pages/index.js
@@ -135,7 +135,7 @@ export default function Home() {
             "The game starts with all the cards face down and players take turns to turn over two cards. If the two cards have the same picture, then they keep the cards, otherwise they turn the cards face down again. The winner is the person with the most cards when all the cards have been taken.",
           images: [
             {
-              url: "/cover.png",
+              url: `${process.env.NEXT_PUBLIC_APP_URL}/cover.png`,
               width: 800,
               height: 600,
               alt: "memorylane-img",
