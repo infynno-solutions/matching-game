4944939c5ff2a9fbb60e7399e4542c4cd2d94494
Meet1501 <meet.infynno@gmail.com>
2023-05-10 20:00:43 +0530

    feat : validation for username

--- src/utils/validationSchema.jsx
+++ src/utils/validationSchema.jsx
@@ -4,5 +4,6 @@ export const userNameSchema = Yup.object().shape({
   username: Yup.string()
     .min(2, "Too Short!")
     .max(70, "Too Long!")
-    .required("Required"),
+    .required("Required")
+    .trim(),
 });
